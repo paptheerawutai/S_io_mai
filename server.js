c65843e87c7fa74a6268683f3610a99230611e8d
DevOAmate <paptheerawutai@gmail.com>
2024-09-20 02:21:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,12 @@
 // const port = process.env.PORT || 5005;
 // server.listen(port, () => console.log(`Server is running on port ${port}`));
 
+
 import express from 'express';
 import http from 'http';
-import  Server  from 'socket.io';
-import axios from 'axios'; // ใช้ axios สำหรับดึงข้อมูลจาก API
-import mqtt from 'mqtt';  // เพิ่ม mqtt
+import { Server } from 'socket.io';
+import axios from 'axios';
+import mqtt from 'mqtt';
 
 const app = express();
 const server = http.createServer(app);
